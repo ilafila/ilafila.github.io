4a582002d51126f7eb8019b3da9295334e3f5119
Ilya Filatov <bob300@inbox.ru>
2020-12-14 15:31:02 +0300

    test

--- client/srcipt.js
+++ client/srcipt.js
@@ -362,15 +362,15 @@ const BookComponent = {
     const returnTime = document.querySelector('.return').value;
     const timeIn = document.querySelector('.timeIn').value;
     const timeOut = document.querySelector('.timeOut').value;
-    console.log(timeIn === '');
-    console.log(timeOut === '');
+    console.log(!timeIn);
+    console.log(!timeOut);
 
     if (departureTime === '' && returnTime === '') {
       alert('Please, filled out all fields!!!');
       return;
     }
 
-    if(timeIn === '' && timeOut === ''){
+    if(!timeIn && !timeOut){
       alert('Please, filled out all fields!!!');
       return;
     }
