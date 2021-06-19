de2c8f8cb06cde1385ef84f3d52252880c313ce0
Ilia Filatov <43479669+ilafila@users.noreply.github.com>
2021-06-19 03:03:44 +0300

    test

--- MessageInput.js
+++ MessageInput.js
@@ -74,8 +74,8 @@ class EmojiPicker {
     render() {
         console.log(this);
         this.getEmoji();
-        const {emotions, gesturesAndPeople, symbols, animalsAndPlants, foodAndDrink,
-            sportsAndActivities, travelAndTransport, items, flags} = this.objectOfEmojiElement;
+        console.log('yeee');
+        const {emotions, gesturesAndPeople, symbols, animalsAndPlants, foodAndDrink, sportsAndActivities, travelAndTransport, items, flags} = this.objectOfEmojiElement;
         const messageInput = new MessageInput();
         return `<div>
                   <div class="emoji-table hide">
